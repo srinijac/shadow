ef33d0fae60536d6ce82d056a15646e85f35c94b
Srinija Cherivirala <srinija@berkeley.edu>
2020-11-22 01:07:06 -0500

    sentiment removing stop words

--- twitter.py
+++ twitter.py
@@ -8,6 +8,9 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
+sent = []
+sent = respond_hello(api)
+
 def main():
     shadowing = api.followers_ids() 
     for id in shadowing: # can use mentions_timeline?
